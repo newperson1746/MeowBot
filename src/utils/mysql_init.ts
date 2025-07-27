e94cbf0a13f42713cd103791ed6af20257a64626
newperson1746 <dcrodrig@andrew.cmu.edu>
2025-07-26 22:48:06 -0400

    modularize mysql init (cleans up index.ts)

---
+++ src/utils/mysql_init.ts
@@ -0,0 +1,103 @@
+import dotenv from 'dotenv-extended';
+import mysql from 'mysql2';
+
+dotenv.load();
+
+// SQL Setup
+// to-do: modularize this whole thing
+const sqlconfig = {
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  database: process.env.SQL_DB,
+  password: process.env.SQL_PASS,
+  connectionLimit: 10
+}
+
+const sqlconfigmc = {
+  host: process.env.SQLMC_HOST,
+  user: process.env.SQLMC_USER,
+  database: process.env.SQLMC_DB,
+  password: process.env.SQLMC_PASS,
+  connectionLimit: 10
+}
+
+const pool = mysql.createPool(sqlconfig, (err) => {
+  if (err) {
+    console.error('Error creating main SQL pool:', err);
+    return;
+  }
+});
+
+const poolmc = mysql.createPool(sqlconfigmc, (err) => {
+  if (err) {
+    console.error('Error creating MC SQL pool:', err);
+    return;
+  }
+});
+
+// Test the pool
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error getting main MySQL pool connection:', err);
+    return;
+  }
+
+  console.log('MySQL main pool connection test successful!');
+
+  // Release the connection back to the pool
+  connection.release();
+});
+
+poolmc.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error getting MC MySQL pool connection:', err);
+    return;
+  }
+
+  console.log('MySQL MC pool connection test successful!');
+
+  // Release the connection back to the pool
+  connection.release();
+});
+
+pool.on('acquire', function (connection) {
+  console.log('MySQL main pool connection %d acquired', connection.threadId);
+  connection.on('error', function (err) {
+    console.error('Error with main MySQL connection:', err);
+    connection.release();
+  });
+});
+
+poolmc.on('acquire', function (connection) {
+  console.log('MySQL MC pool connection %d acquired', connection.threadId);
+  connection.on('error', function (err) {
+    console.error('Error with MC MySQL connection:', err);
+    connection.release();
+  });
+});
+
+function getMySQLVersion() {
+  pool.query('SELECT version()', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const mysqlVersion = results[0]['version()'];
+    console.log(`main MySQL Version: ${mysqlVersion}`);
+  });
+}
+
+function getMySQLVersionMC() {
+  poolmc.query('SELECT version()', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const mysqlVersion = results[0]['version()'];
+    console.log(`MC MySQL Version: ${mysqlVersion}`);
+  });
+}
+
+export { pool, poolmc, getMySQLVersion, getMySQLVersionMC };
