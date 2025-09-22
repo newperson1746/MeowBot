22fd2ca452c767368075fa9590721f04d1fa9622
newperson1746 <dcrodrig@andrew.cmu.edu>
2025-09-21 21:24:27 -0400

    fix ordering of async stuff for sohopplrm

--- src/commands/sohopplrm.ts
+++ src/commands/sohopplrm.ts
@@ -41,68 +41,67 @@ export default class SohoPplRm {
         console.log('Error in Soho people get query:', error);
       } else {
         stats = results[0];
+        sqlRemoveSohoPeople(member.id.toString(), (error, result) => {
+          if (error) {
+            console.log('Error in SoHo people remove query:', error);
+            stdembed.setDescription(`Error in sqlRemoveSohoPeople: ${error}`);
+            interaction.followUp({
+              embeds: [stdembed],
+              ephemeral: false
+            })
+          } else {
+            if (result.affectedRows === 0) {
+              public_success.setDescription(
+                `${interaction.member.user}, you weren’t on the SoHo people list.`
+              );
+            } else {
+              const timeIn = stats.time;
+              const timeOut = Math.floor(Date.now() / 1000)
+              let totaltime;
+              sqlGetSohoStreak(member.id.toString(), (error, results, fields) => {
+                if (error) {
+                  console.log('Error in Soho streaks get query:', error);
+                } else {
+                  if (results.length === 0) {
+                    // The user does not have a streak yet
+                    totaltime = 0;
+                  } else {
+                    // there is at least one row in the result (the user)
+                    totaltime = results[0].totaltime;
+                  }
+
+                  const elapsed = timeOut - timeIn;
+
+                  // Everything is calculated, let's add the new value
+                  sqlWriteSohoStreak(member.id.toString(), elapsed.toString(), (error, result) => {
+                    if (error) {
+                      console.log('Error in Soho streaks write query:', error);
+                    }
+                  });
+
+                  const humanElapsed = humanizeDuration(elapsed * 1000, {largest: 2, round: true});
+                  const humanStreak = humanizeDuration(totaltime * 1000, {largest: 2, round: true});
+
+                  public_success.setDescription(
+                    `${interaction.member.user}, successfully removed yourself from the SoHo people list\n\n` +
+                    `You entered Soho at <t:${timeIn}:f> \n` +
+                    `You left Soho at <t:${timeOut}:f> \n\n` +
+                    `Time elapsed this visit: ${humanElapsed}` +
+                    `Current Soho streak time: ${humanStreak}`
+                  );
+
+                  interaction.followUp({
+                    embeds: [public_success],
+                  });
+                }
+              });
+        
+            }
+          }
+
+        })
       }
     });
-
-      sqlRemoveSohoPeople(member.id.toString(), (error, result) => {
-        if (error) {
-          console.log('Error in SoHo people remove query:', error);
-          stdembed.setDescription(`Error in sqlRemoveSohoPeople: ${error}`);
-          interaction.followUp({
-            embeds: [stdembed],
-            ephemeral: false
-          })
-        } else {
-          if (result.affectedRows === 0) {
-            public_success.setDescription(
-              `${interaction.member.user}, you weren’t on the SoHo people list.`
-            );
-          } else {
-            const timeIn = stats.time;
-            const timeOut = Math.floor(Date.now() / 1000)
-            let totaltime;
-            sqlGetSohoStreak(member.id.toString(), (error, results, fields) => {
-              if (error) {
-                console.log('Error in Soho streaks get query:', error);
-              } else {
-                if (results.length === 0) {
-                  // The user does not have a streak yet
-                  totaltime = 0;
-                } else {
-                  // there is at least one row in the result (the user)
-                  totaltime = results[0].totaltime;
-                }
-
-                const elapsed = timeOut - timeIn;
-
-                // Everything is calculated, let's add the new value
-                sqlWriteSohoStreak(member.id.toString(), elapsed.toString(), (error, result) => {
-                  if (error) {
-                    console.log('Error in Soho streaks write query:', error);
-                  }
-                });
-
-                const humanElapsed = humanizeDuration(elapsed * 1000, {largest: 2, round: true});
-                const humanStreak = humanizeDuration(totaltime * 1000, {largest: 2, round: true});
-
-                public_success.setDescription(
-                  `${interaction.member.user}, successfully removed yourself from the SoHo people list\n\n` +
-                  `You entered Soho at <t:${timeIn}:f> \n` +
-                  `You left Soho at <t:${timeOut}:f> \n\n` +
-                  `Time elapsed this visit: ${humanElapsed}` +
-                  `Current Soho streak time: ${humanStreak}`
-                );
-              }
-            });
-      
-          }
-        }
-
-        interaction.followUp({
-          embeds: [public_success],
-        });
-
-      })
     })
   }
 }
